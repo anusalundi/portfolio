1df57f2735eb0c6aede63176d7b7355f758ba6ae
Anu <159231883+anusalundi@users.noreply.github.com>
2024-06-23 15:21:54 +0300

    Create Cheese.java

---
+++ CheeseShop/Cheese.java
@@ -0,0 +1,31 @@
+public class Cheese {
+    private int id;
+    private String name;
+    private int cost;
+
+    public Cheese(int id, String name, int cost) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
