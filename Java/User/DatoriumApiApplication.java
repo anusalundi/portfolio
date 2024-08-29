1845d53e47ffb6144693c9783f84b14a3d94dd42
Anu <159231883+anusalundi@users.noreply.github.com>
2024-08-29 16:20:40 +0300

    Create DatoriumApiApplication.java

---
+++ Java/User/DatoriumApiApplication.java
@@ -0,0 +1,14 @@
+package com.datorium.Datorium.API;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@CrossOrigin
+public class DatoriumApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("asd");
+		SpringApplication.run(DatoriumApiApplication.class, args);
+	}
+}
