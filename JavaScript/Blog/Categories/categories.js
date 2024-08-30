2d65764de12d6b4575b09fc4cef43e6e7d77127f
Anu <159231883+anusalundi@users.noreply.github.com>
2024-08-30 11:27:24 +0300

    Create categories.js

---
+++ JavaScript/Blog/Categories/categories.js
@@ -0,0 +1,19 @@
+const categories = [
+    {
+        id: 1,
+        title: 'Kokandus',
+        postId: 1,
+    },
+    {
+        id: 2,
+        title: 'Reisikirjad',
+        postId: 2,
+    },
+    {
+        id: 3,
+        title: 'Sisekujundus',
+        postId: 3,
+    },
+]
+
+module.exports = categories;
