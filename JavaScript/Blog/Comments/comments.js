d44f5f703caf96ce8f8a1c3ace4d14f17a1960f3
Anu <159231883+anusalundi@users.noreply.github.com>
2024-08-30 11:29:28 +0300

    Create comments.js

---
+++ JavaScript/Blog/Comments/comments.js
@@ -0,0 +1,22 @@
+const comments = [
+    {
+        id: 1,
+        userId: 3,
+        postId: 1,
+        content: 'Väga hea postitus. Jään järgmist ootama :)',
+    },
+    {
+        id: 2,
+        userId: 2,
+        postId: 2,
+        content: 'Oo, rõõm kuulda, et ilusad ilmad jätkuvad :D',
+    },
+    {
+        id: 3,
+        userId: 3,
+        postId: 3,
+        content: 'Nii head ideed, aitäh!',
+    },
+]
+
+module.exports = comments;
