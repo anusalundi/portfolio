948b9577ee17e18e0c6ca0b4b3bd71055ebb1781
Anu <159231883+anusalundi@users.noreply.github.com>
2024-09-02 09:58:31 +0300

    Create UserController.java

---
+++ Java/People/UserController.java
@@ -0,0 +1,19 @@
+package com.datorium.Datorium.API.Controllers;
+
+import com.datorium.Datorium.API.DTOs.User;
+import com.datorium.Datorium.API.Services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+    public UserController(){
+        userService = new UserService();}
+
+    @PostMapping("/add")
+    public int add(@RequestBody User user){
+        return userService.add(user);
+    }
+}
