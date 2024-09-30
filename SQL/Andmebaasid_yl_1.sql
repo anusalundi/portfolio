be48e15ab8c32f21a24c34fa4217d16567f0a6a1
Anu <159231883+anusalundi@users.noreply.github.com>
2024-09-30 19:29:23 +0300

    Update Andmebaasid_yl_1.sql

--- SQL/Andmebaasid_yl_1.sql
+++ SQL/Andmebaasid_yl_1.sql
@@ -34,17 +34,21 @@ Order BY pikkus;
 SELECT magaja_id, (eesnimi & ' ' & perenimi) AS nimi
 FROM Magaja;
 
+--9--
 SELECT DISTINCT perenimi
 FROM Magaja;
 
+--10--
 SELECT *
 FROM Magaja
 WHERE eesnimi='Jaan';
 
+--11--
 SELECT *
 FROM Magaja
 WHERE eesnimi<>'Jaan';
 
+--12--
 SELECT *
 FROM Magaja
 WHERE eesnimi='Jaan' OR perenimi='Jaan';
@@ -53,6 +57,7 @@ SELECT *
 FROM Magaja
 WHERE perenimi LIKE 'J__n%';
 
+--13--
 SELECT * FROM Auto
 WHERE registri_nr NOT LIKE '[0-9][0-9][0-9][a-z][a-z][a-z]'; 
 
@@ -64,10 +69,84 @@ SELECT *
 FROM Magaja
 WHERE perenimi LIKE '%[?_]%';
 
+--14--
 SELECT *
 FROM Magaja
 WHERE perenimi NOT LIKE 'K%' AND len(perenimi)=4;
 
+--15/16--
 SELECT *
 FROM Magaja
 WHERE eesnimi IS NULL;
+
+--17--
+SELECT *
+FROM Magaja
+WHERE eesnimi IN ('Jaan', 'Jüri'); 
+
+SELECT *
+FROM Magaja
+WHERE eesnimi ='Jaan' OR eesnimi='Jüri';
+
+--18--
+SELECT *
+FROM Magaja
+WHERE eesnimi NOT IN ('Jaan', 'Jüri');
+
+SELECT *
+FROM Magaja
+WHERE NOT (eesnimi ='Jaan' OR eesnimi='Jüri');
+
+--19--
+SELECT *
+FROM Ase
+WHERE pikkus BETWEEN 210 AND 270;
+
+SELECT *
+FROM Ase
+WHERE pikkus>= 210 AND pikkus<=270; 
+
+--20--
+SELECT *
+FROM Ase
+WHERE pikkus NOT BETWEEN 210 AND 270;
+
+SELECT *
+FROM Ase
+WHERE NOT (pikkus BETWEEN 210 AND 270);
+
+--21--
+SELECT *
+FROM Ase
+WHERE (pikkus NOT BETWEEN 210 AND 270) OR (pikkus IS NULL);
+
+--22--
+SELECT *
+FROM Magaja
+WHERE (synni_aeg>#1976-01-01# AND sugu='M') OR
+(synni_aeg>#1979-01-01# AND sugu='N' AND (telefon IS NOT NULL OR
+aadress LIKE 'Tallinn%'))
+ORDER BY synni_aeg; 
+
+--23--
+SELECT *
+FROM Magaja
+WHERE magaja_id NOT IN
+(SELECT magaja_id
+FROM Magaja
+WHERE (synni_aeg>#1976-01-01# AND sugu='M') OR
+(synni_aeg>#1979-01-01# AND sugu='N' AND (telefon IS NOT
+NULL OR aadress LIKE 'Tallinn%'))); 
+
+--24--
+SELECT *
+FROM Ase
+WHERE (pikkus>270 AND laius<=180) OR (pikkus<=270 AND laius>180);
+
+--25. esita erinevad perenimed suurtähtedega uues lahtris--
+SELECT DISTINCT UCase(perenimi) AS [Perenimi suurtähtedega]
+FROM Magaja;
+
+--29. keskmine vanus aastates--
+SELECT Avg(Date()-synni_aeg)/365.25 AS keskmine_vanus_aastates
+FROM Magaja;
