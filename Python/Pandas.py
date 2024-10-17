57dd2a38175582a55e890592cf0695f6b33de035
Anu <159231883+anusalundi@users.noreply.github.com>
2024-10-17 09:27:24 +0300

    Update Pandas.py

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -40,3 +40,33 @@ g7_pop.iloc[-1]
 g7_pop[['Italy', 'France']]
 g7_pop.iloc[[0, 1]]
 g7_pop['Canada': 'Italy']
+
+## Conditional selection (boolean arrays / series)
+g7_pop * 1_000_000
+g7_pop > 70
+g7_pop[g7_pop > 70]
+g7_pop.mean()
+g7_pop[g7_pop > g7_pop.mean()]
+g7_pop.std()
+g7_pop[(g7_pop > g7_pop.mean() - g7_pop.std() / 2) | (g7_pop > g7_pop.mean() + g7_pop.std() / 2)]
+
+## Operations and methods
+g7_pop
+g7_pop * 1_000_000
+g7_pop.mean()
+np.log(g7_pop)
+g7_pop['France': 'Italy'].mean()
+
+## Boolean arrays
+g7_pop
+g7_pop > 80
+g7_pop[g7_pop > 80]
+g7_pop[(g7_pop > 80) | (g7_pop < 40)]
+g7_pop[(g7_pop > 80) & (g7_pop < 200)]
+
+## Modifying series
+g7_pop['Canada'] = 40.5
+g7_pop.iloc[-1] = 500
+g7_pop
+g7_pop[g7_pop < 70] = 99.99
+g7_pop
