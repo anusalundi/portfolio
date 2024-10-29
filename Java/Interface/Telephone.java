f89652bfac7433a5d55f3d9e2c47a31e55c1e556
Anu <159231883+anusalundi@users.noreply.github.com>
2024-10-29 16:31:25 +0200

    Create Telephone.java

---
+++ Java/Interface/Telephone.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface Telephone {
+
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+
+}
