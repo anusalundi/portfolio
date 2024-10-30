13c7f553c94f09e0405496b274a7c14ddf8826cd
Anu <159231883+anusalundi@users.noreply.github.com>
2024-10-30 13:48:06 +0200

    Create SpringnaideApplication.java

---
+++ Java/Spring/SpringnaideApplication.java
@@ -0,0 +1,13 @@
+package ee.naidis.springnaide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringnaideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnaideApplication.class, args);
+	}
+}
