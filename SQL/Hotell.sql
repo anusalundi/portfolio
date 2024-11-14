475a6c7bf3fa4ba8b4615a9a02b5cefdfb9566fd
Anu <159231883+anusalundi@users.noreply.github.com>
2024-11-14 12:04:07 +0200

    Update Hotell.sql

--- SQL/Hotell.sql
+++ SQL/Hotell.sql
@@ -27,3 +27,40 @@ WHERE Hotell.hotelli_nr=Ruum.hotelli_nr AND Ruum.hind>300;
 SELECT Reserveerimine.*, Hotell.nimi AS hotelli_nimi, Külaline.perenimi
 FROM Külaline INNER JOIN (Reserveerimine INNER JOIN Hotell ON Reserveerimine.hotelli_nr=Hotell.hotelli_nr) ON Külaline.külalise_nr=Reserveerimine.külalise_nr
 ORDER BY Hotell.nimi DESC , Külaline.külalise_nr;
+
+SELECT Hotell.hotelli_nr, Hotell.nimi, Count(Reserveerimine.hotelli_nr) AS arv
+FROM Hotell LEFT JOIN Reserveerimine ON Hotell.hotelli_nr=Reserveerimine.hotelli_nr
+GROUP BY Hotell.hotelli_nr, Hotell.nimi
+ORDER BY Count(Reserveerimine.hotelli_nr) DESC;
+
+SELECT *
+FROM Hotell
+WHERE hotelli_nr <>ALL (SELECT hotelli_nr FROM Reserveerimine WHERE hotelli_nr IS NOT NULL);
+
+SELECT DISTINCT eesnimi
+FROM Külaline;
+
+SELECT külalise_nr
+FROM Reserveerimine
+GROUP BY külalise_nr
+HAVING Count(*)>2;
+
+SELECT Külaline.külalise_nr, Külaline.eesnimi, Külaline.perenimi, Külaline.aadress, Count(Reserveerimine.külalise_nr) AS arv
+FROM Külaline INNER JOIN Reserveerimine ON Külaline.külalise_nr=Reserveerimine.külalise_nr
+GROUP BY Külaline.külalise_nr, Külaline.eesnimi, Külaline.perenimi, Külaline.aadress
+HAVING Count(Reserveerimine.külalise_nr)>=ALL(SELECT Count(*) AS arv FROM Reserveerimine GROUP BY külalise_nr);
+
+SELECT K.külalise_nr, K.perenimi, H.hotelli_nr, H.nimi
+FROM Külaline AS K, Hotell AS H
+WHERE NOT EXISTS (SELECT * FROM Reserveerimine AS RE
+WHERE RE.külalise_nr=K.külalise_nr AND RE.hotelli_nr=H.hotelli_nr)
+ORDER BY K.külalise_nr;
+
+SELECT Count(eesnimi) AS arv
+FROM (SELECT DISTINCT eesnimi FROM Külaline)  AS erinevad_eesnimed;
+
+SELECT alguse_aeg AS kuupäev
+FROM Reserveerimine
+UNION SELECT lopu_aeg 
+FROM Reserveerimine
+ORDER BY kuupäev DESC;
