8b58ce89034eeab67bd302f6147949f1f4bc1c6f
Anu <159231883+anusalundi@users.noreply.github.com>
2024-11-19 14:11:34 +0200

    Update Tudeng.sql

--- SQL/Tudeng.sql
+++ SQL/Tudeng.sql
@@ -1,4 +1,5 @@
-SELECT aine_kood, nimetus AS NIMETUS, kommentaar, punkte
+--MS Access--
+SELECT aine_kood, Ucase(nimetus) AS NIMETUS, kommentaar, punkte
 FROM Aine
 WHERE (aine_kood LIKE 'F%' OR aine_kood LIKE 'H%')
 AND aine_kood NOT LIKE '%2'
@@ -6,9 +7,25 @@ AND kommentaar IS NOT NULL
 AND ((punkte>=1 AND punkte<=2) OR punkte>3)
 ORDER BY punkte ASC, nimetus DESC;
 
-SELECT aine_kood, nimetus AS NIMETUS, kommentaar, punkte
+SELECT aine_kood, Ucase(nimetus) AS NIMETUS, kommentaar, punkte
 FROM Aine
 WHERE aine_kood LIKE '[FH]%[!2]'
+AND kommentaar IS NOT NULL
+AND (punkte BETWEEN 1 AND 2 OR punkte>3)
+ORDER BY punkte, nimetus DESC;
+
+--PostgreSQL--
+SELECT aine_kood, Upper(nimetus) AS NIMETUS, kommentaar, punkte
+FROM Aine
+WHERE (aine_kood ILIKE 'F%' OR aine_kood ILIKE 'H%')
+AND aine_kood NOT LIKE '%2'
+AND kommentaar IS NOT NULL
+AND ((punkte>=1 AND punkte<=2) OR punkte>3)
+ORDER BY punkte ASC, nimetus DESC;
+
+SELECT aine_kood, Upper(nimetus) AS NIMETUS, kommentaar, punkte
+FROM Aine
+WHERE aine_kood ~* '^[FH].*[^2]$'
 AND kommentaar IS NOT NULL
 AND (punkte BETWEEN 1 AND 2 OR punkte>3)
 ORDER BY punkte, nimetus DESC;
